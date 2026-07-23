64a1874f9c6385cc6e25fb0171cf850f2d2e658a
Yuri Kiev de Sousa Barreto <yurikievbarreto@gmail.com>
2026-07-23 18:42:46 -0300

    refactor: separa frontend da API

--- app/main.py
+++ app/main.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 from fastapi import APIRouter
 from .api.main_router import mainRouter
 from contextlib import asynccontextmanager
-from app.models.usuario import Usuario
-from fastapi.staticfiles import StaticFiles
 from app.core.database import init_db
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse, HTMLResponse
 from pathlib import Path
+from fastapi.middleware.cors import CORSMiddleware
 
 
 import uvicorn
@@ -37,13 +34,13 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-from fastapi.middleware.cors import CORSMiddleware
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5500",    # Live Server (comum no VS Code)
-        "http://127.0.0.1:5500",
+        "http://localhost:5501",    # Live Server (comum no VS Code)
+        "http://127.0.0.1:5501",
         "http://localhost:8000",    # Seu front rodando no Docker/Local
         "http://127.0.0.1:8000",
         "https://yurikievbarreto.github.io", # URL do seu projeto no GitHub Pages
@@ -54,24 +51,6 @@ app.add_middleware(
 )
 
 app.include_router(mainRouter)
-
-app.mount(
-    "/static",
-    StaticFiles(directory=Path("app/static")), 
-    name="static"
-)
-
-
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-async def serve_dashboard():
-    """Serve o arquivo HTML principal quando o usuário acessa a raiz."""
-    # Garanta que o caminho para o seu arquivo HTML esteja correto
-    dashboard_path = Path("app/static/index.html")
-    
-    if not dashboard_path.is_file():
-        return HTMLResponse("<h1>Erro 404: Arquivo dashboard.html não encontrado.</h1>")
-        
-    return FileResponse(dashboard_path)
 
 
 
--- frontend/utils.js
+++ frontend/utils.js
@@ -1,3 +1,3 @@
 const endereco_local = "http://127.0.0.1:8000"
 const endereco_aws = "edereco aws"
-export const endereco_em_uso = endereco_local
+export const endereco_api_em_uso = endereco_local
