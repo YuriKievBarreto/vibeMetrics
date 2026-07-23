075ad054ba3d219b9c238de01695212901735da3
Yuri Kiev de Sousa Barreto <yurikievbarreto@gmail.com>
2026-07-23 17:59:44 -0300

    refactor(crud): cria diretorio 'repositories' e reorganzia arquivos de logica relacionada ao crud das entidades

---
+++ app/repositories/usuario_top_artista_repository.py
@@ -0,0 +1,34 @@
+from app.schemas.schema_faixa import FaixaCreate
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.database import async_engine
+from app.models.usuario_top_faixa import UsuarioTopFaixa
+from app.models.usuario_top_artista import UsuarioTopArtista
+from app.models.faixa import Faixa  
+from sqlalchemy.orm import joinedload
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from typing import Type, List, Dict
+
+
+
+async def ler_usuario_top_artistas(id_usuario: str, quantidade:str=None):
+   async with AsyncSession(async_engine) as db:
+      print("iniciando busca de relacionamentos")
+      stmt = select(UsuarioTopArtista).where(
+         UsuarioTopArtista.id_usuario == id_usuario
+      ).options(
+         joinedload(UsuarioTopArtista.artista) 
+      ).order_by(
+        UsuarioTopArtista.short_time_rank.asc()
+    ).limit(quantidade)
+      
+   result =  await db.execute(stmt)
+   return result.scalars().all()
+
+
+
+
+
+
+
--- app/repositories/usuario_top_faixa_repository.py
+++ app/repositories/usuario_top_faixa_repository.py
@@ -1,7 +1,8 @@
 from app.schemas.schema_faixa import FaixaCreate
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import async_engine
-from app.models.relacionamentos import UsuarioTopArtista, UsuarioTopFaixa
+from app.models.usuario_top_faixa import UsuarioTopFaixa
+from app.models.usuario_top_artista import UsuarioTopArtista
 from app.models.faixa import Faixa  
 from sqlalchemy.orm import joinedload
 from sqlalchemy import select
@@ -22,23 +23,3 @@ async def ler_usuario_top_faixas(id_usuario: str, quantidade: int=None):
       
    result =  await db.execute(stmt)
    return result.scalars().all()
-
-
-async def ler_usuario_top_artistas(id_usuario: str, quantidade:str=None):
-   async with AsyncSession(async_engine) as db:
-      print("iniciando busca de relacionamentos")
-      stmt = select(UsuarioTopArtista).where(
-         UsuarioTopArtista.id_usuario == id_usuario
-      ).options(
-         joinedload(UsuarioTopArtista.artista) 
-      ).order_by(
-        UsuarioTopArtista.short_time_rank.asc()
-    ).limit(quantidade)
-      
-   result =  await db.execute(stmt)
-   return result.scalars().all()
-
-
-
-
-
